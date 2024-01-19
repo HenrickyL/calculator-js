9d1bc8bc807d7a393729408c49f14b32fcae2a76
Dio <49930685+dionissa@users.noreply.github.com>
2024-01-19 20:48:44 -0300

    Mobile and Visual Update

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-
-
-const display = document.getElementById('display')
-const operationArray = ['+','-','*','/', '^']
+const display = document.querySelector(".value");
+const history = document.getElementById("history");
+const operationArray = ['+', '-', '*', '/', '^'];
 let calculationHistory = [];
-window.addEventListener('load',()=>{
-    display.textContent = 0
-})
+
+window.addEventListener('load', () => {
+  display.value = '0';
+});
+
+function updateHistory() {
+    history.value = calculationHistory.join('\n');
+    history.scrollTop = history.scrollHeight;  // Scroll to the bottom
+  }
 
 function getDisplay(){
-    return display.textContent
+    return display.value
 }
 
 function getDisplayLast(){
-    const len = display.textContent.length
-    return display.textContent[len-1]
-}
-
-function updateHistory() {
-    history.innerHTML = calculationHistory.map((calculation, index) => `<div key=${index}>${calculation}</div>`).join('');
+    const len = display.value.length
+    return display.value[len-1]
 }
 
 function isOperation(character){
@@ -25,32 +26,31 @@ function isOperation(character){
 }
 
 function clearDisplay(){
-    display.textContent = '0'
+    display.value = '0'
 }
 
 function deleteLast(){
-    display.textContent = display.textContent.slice(0,-1)
+    display.value = display.value.slice(0,-1)
 }
 
 function appendCharacter(character){
     let result = character
     if(getDisplay() == '0' ){
-        display.textContent = ''
+        display.value = ''
     }
-    display.textContent += result
+    display.value += result
 }
 
-
-function calculateResult(){
-    const result = eval(display.textContent)
-    calculationHistory.push(`${display.textContent} = ${result}`);
-    display.textContent = result
-    updateHistory();
-}
+    function calculateResult() {
+      const result = eval(display.value);
+      calculationHistory.push(`${display.value} = ${result}`);
+      display.value = result;
+      updateHistory();
+    }
 
 
 function appendOperation(op){
     if(!isOperation(getDisplayLast())){
-        display.textContent += op
+        display.value += op
     }
 }
